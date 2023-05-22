fb5639a05765b2cd241aaf4d5729cf5c9acdd692
Marius C <marius.ciucam@gmail.com>
2023-05-22 14:26:20 +0300

    FEAT: Add tests for wsConnectorFactory

---
+++ factory/wsConnectorFactory_test.go
@@ -0,0 +1,54 @@
+package factory
+
+import (
+	"testing"
+
+	"github.com/multiversx/mx-chain-ws-connector-template-go/config"
+	"github.com/stretchr/testify/require"
+)
+
+func createConfig() config.WebSocketConfig {
+	return config.WebSocketConfig{
+		Url:                "localhost",
+		MarshallerType:     "json",
+		RetryDuration:      1,
+		WithAcknowledge:    false,
+		BlockingAckOnError: false,
+		HasherType:         "blake2b",
+	}
+}
+
+func TestCreateWSConnector(t *testing.T) {
+	t.Parallel()
+
+	t.Run("invalid marshaller, should return error", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := createConfig()
+		cfg.MarshallerType = "invalid"
+		ws, err := CreateWSConnector(cfg)
+		require.NotNil(t, err)
+		require.Nil(t, ws)
+	})
+
+	t.Run("cannot create ws host, should return error", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := createConfig()
+		cfg.RetryDuration = 0
+		ws, err := CreateWSConnector(cfg)
+		require.NotNil(t, err)
+		require.Nil(t, ws)
+	})
+
+	t.Run("should work", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := createConfig()
+		ws, err := CreateWSConnector(cfg)
+		require.Nil(t, err)
+		require.NotNil(t, ws)
+
+		_ = ws.Close()
+	})
+}
